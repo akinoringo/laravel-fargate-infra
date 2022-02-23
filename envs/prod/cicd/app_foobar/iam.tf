a838145d46488cc887bd0f98a196477fdbfd9c9d
akinoringo <wbraver@outlook.jp>
2022-02-23 11:49:22 +0900

    deployerのiamにassume_roleおよびtagsettionのポリシーを設定

--- envs/prod/cicd/app_foobar/iam.tf
+++ envs/prod/cicd/app_foobar/iam.tf
@@ -5,3 +5,27 @@ resource "aws_iam_user" "github" {
     Name = "${local.name_prefix}-${local.service_name}-github"
   }
 }
+
+resource "aws_iam_role" "deployer" {
+  name = "${local.name_prefix}-${local.service_name}-deployer"
+
+  assume_role_policy = jsonencode(
+    {
+      "Version" : "2012-10-17",
+      "Statement" : [
+        {
+          "Effect" : "Allow",
+          "Action" : [
+            "sts:AssumeRole",
+            "sts:TagSession"
+          ],
+          "Principal" : {
+            "AWS" : aws_iam_user.github.arn
+          }
+        }
+      ]
+    }
+  )
+
+  tags = "${local.name_prefix}-${local.service_name}-deployer"
+}
